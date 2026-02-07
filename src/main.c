c960ec31c6eb95ffa427185fe7ffe138ca47e643
Adran Carnavale <adran.carnavale@gmail.com>
2026-02-07 10:28:56 -0300

    feat: draw a sphere on the screen

--- src/main.c
+++ src/main.c
@@ -3,18 +3,25 @@
 #define SCREEN_WIDTH 1000
 #define SCREEN_HEIGHT 750
 
+typedef struct {
+  Vector3 position;
+  float radius;
+  Color color;
+} Sphere;
+
 int main(void) {
   InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Raytracer");
 
   Color pixels[SCREEN_HEIGHT][SCREEN_WIDTH];
 
-  for (int x = 0; x < SCREEN_WIDTH; x++) {
-    for (int y = 0; y < SCREEN_HEIGHT; y++) {
-      pixels[y][x] = RED;
-    };
-  };
+  Sphere sphere = {.position = {0, 0, 10}, .radius = 2.f, .color = RED};
+  Camera camera = {.position = {0, 0, 0},
+                   .target = {0, 0, 10},
+                   .up = {0, 1, 0},
+                   .fovy = 60,
+                   .projection = CAMERA_PERSPECTIVE};
 
-  Image image = {.data = &pixels,
+  Image image = {.data = pixels,
                  .width = SCREEN_WIDTH,
                  .height = SCREEN_HEIGHT,
                  .mipmaps = 1,
@@ -24,6 +31,17 @@ int main(void) {
 
   while (!WindowShouldClose()) {
     BeginDrawing();
+
+    for (int x = 0; x < SCREEN_WIDTH; x++) {
+      for (int y = 0; y < SCREEN_HEIGHT; y++) {
+        Ray ray = GetScreenToWorldRay((Vector2){x, y}, camera);
+        RayCollision collision =
+            GetRayCollisionSphere(ray, sphere.position, sphere.radius);
+        pixels[y][x] = collision.hit ? sphere.color : BLACK;
+      };
+    };
+
+    UpdateTexture(texture, pixels);
     DrawTexture(texture, 0, 0, WHITE);
     EndDrawing();
   };
